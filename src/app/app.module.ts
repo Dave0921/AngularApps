e68a9b2bb82ef9a09f7fbb44cace938304a4bb5d
DavidS <sdave0921@gmail.com>
2018-02-18 20:41:37 -0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 
 import { WebsocketService } from './websocket.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChatFormComponent } from './chat-form/chat-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFormComponent
   ],
   imports: [
     NgbModule.forRoot(),
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [WebsocketService],
   bootstrap: [AppComponent]
---
+++ src/app/chat-form/chat-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-form',
+  templateUrl: './chat-form.component.html',
+  styleUrls: ['./chat-form.component.css']
+})
+export class ChatFormComponent implements OnInit{
+  @Input()
+
+  model = {
+    left: true,
+    middle: false,
+    right: false,
+  };
+  constructor() {}
+  ngOnInit(){}
+}
